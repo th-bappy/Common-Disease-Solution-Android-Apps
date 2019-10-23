f9289dad05fe8861efdf0499392039208a17ff99
tanvirbappy <55150759+thbappygit@users.noreply.github.com>
2019-10-23 14:56:51 +0600

    Add files via upload

---
+++ Disease_3.java
@@ -0,0 +1,13 @@
+package com.example.commondisease;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Disease_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disease_3);
+    }
+}
---
+++ app/src/main/java/com/example/commondisease/Disease_5.java
@@ -0,0 +1,13 @@
+package com.example.commondisease;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Disease_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disease_5);
+    }
+}
